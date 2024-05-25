8fc81a082a3fbdfa37e4c1b092f160bb6d3ba5e7
Bushra <bshra.m@sybertechnology.com>
2024-05-25 20:09:35 +0200

    Added city input event handler

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
-import './App.css';
+import './Styles/App.css';
 import MainWeatherWindow from './Components/MainWeatherWindow';
 import CityInput from './Components/CityInput';
 import WeatherBox from './Components/WeatherBox';
 
 class App extends React.Component {
+  state = {
+    city: 'city name'
+  }
+  setCity = (e) => {
+    this.setState({ city: e.target.value });
+  }
   render() {
     return (
       <div className='App'>
         <header className='App-header'>
-          <MainWeatherWindow test="test">
-            <CityInput />
+          <MainWeatherWindow test={this.state.city}>
+            <CityInput setCity={this.setCity.bind(this)} />
             <ul className='weather-box-list'>
-              <li><WeatherBox/></li>
-              <li><WeatherBox/></li>
-              <li><WeatherBox/></li>
-              <li><WeatherBox/></li>
-              <li><WeatherBox/></li>
+              <li><WeatherBox /></li>
+              <li><WeatherBox /></li>
+              <li><WeatherBox /></li>
+              <li><WeatherBox /></li>
+              <li><WeatherBox /></li>
             </ul>
           </MainWeatherWindow>
         </header>
--- src/Components/CityInput.js
+++ src/Components/CityInput.js
@@ -2,11 +2,30 @@ import React from "react";
 import "../Styles/CityInput.css"
 
 export default class CityInput extends React.Component {
+    state = {
+        isCitySet: false
+    }
     render(props) {
+        const onClickHandler = (e) => {
+            const eventKey = e.which? e.which: e.keyCode;
+            if(eventKey === 13){
+                if(/^[a-zA-Z]+$/.test(e.target.value)){
+                    this.props.setCity(e);
+                    this.setState({isCitySet: true});
+                    e.target.value="";
+                    e.target.placeholder="Enter a city";
+                } else {
+                    e.target.value="";
+                    e.target.placeholder="Please enter a valid city name"
+                }
+            }
+        }
         return (
             <input className="city-input"
                 type="text"
                 placeholder="Enter a city"
+                style={{top: this.state.isCitySet? '-400px': '78px'}}
+                onKeyDown={onClickHandler}
             />
         )
     }
